55bf322fd01d02917ed47d38ee60dcfd5ba834d1
Arthur Zhang <zhangwfkl@gmail.com>
2024-11-20 11:49:34 +0800

    Chore: RedHat:

--- RedHat-installer.sh
+++ RedHat-installer.sh
@@ -5,10 +5,31 @@ then
         mkdir app
 fi
 
-cd app && \
-	curl -OL https://download.docker.com/linux/rhel/8/x86_64/stable/Packages/containerd.io-1.7.23-3.1.el8.x86_64.rpm &&\
-	curl -OL https://download.docker.com/linux/rhel/8/x86_64/stable/Packages/docker-buildx-plugin-0.17.1-1.el8.x86_64.rpm && \
-	curl -OL https://download.docker.com/linux/rhel/8/x86_64/stable/Packages/docker-ce-27.3.1-1.el8.x86_64.rpm &&\
-	curl -OL https://download.docker.com/linux/rhel/8/x86_64/stable/Packages/docker-ce-cli-27.3.1-1.el8.x86_64.rpm &&\
-	curl -OL https://download.docker.com/linux/rhel/8/x86_64/stable/Packages/docker-compose-plugin-2.29.7-1.el8.x86_64.rpm &&
-	sudo dnf install containerd.io-1.7.23-3.1.el8.x86_64.rpm docker-buildx-plugin-0.17.1-1.el8.x86_64.rpm docker-ce-27.3.1-1.el8.x86_64.rpm docker-ce-cli-27.3.1-1.el8.x86_64.rpm docker-compose-plugin-2.29.7-1.el8.x86_64.rpm
+containerd=containerd.io-1.7.23-3.1.el8.x86_64
+buildx=docker-buildx-plugin-0.17.1-1.el8.x86_64
+docker_ce=docker-ce-27.3.1-1.el8.x86_64
+docker_ce_cli=docker-ce-cli-27.3.1-1.el8.x86_64
+docker_compose=docker-compose-plugin-2.29.7-1.el8.x86_64
+cd app
+
+if [[ ! -f ${containerd}.rpm ]];
+then
+	curl -OL https://download.docker.com/linux/rhel/8/x86_64/stable/Packages/${containerd}.rpm
+fi
+
+if [[ ! -f ${buildx}.rpm ]]; then
+	curl -OL https://download.docker.com/linux/rhel/8/x86_64/stable/Packages/${buildx}.rpm 
+fi
+
+if [[ ! -f ${docker_ce}.rpm ]]; then
+	curl -OL https://download.docker.com/linux/rhel/8/x86_64/stable/Packages/${docker_ce}.rpm 
+fi
+
+if [[ ! -f ${docker_ce_cli}.rpm ]]; then
+	curl -OL https://download.docker.com/linux/rhel/8/x86_64/stable/Packages/${docker_ce_cli}.rpm 
+fi
+
+if [[ ! -f ${docker_compose}.rpm ]]; then
+	curl -OL https://download.docker.com/linux/rhel/8/x86_64/stable/Packages/${docker_compose}.rpm 
+fi
+	sudo dnf install ${containerd}.rpm ${buildx}.rpm ${docker-ce}.rpm ${docker-ce-cli}.rpm ${docker-compose}.rpm
