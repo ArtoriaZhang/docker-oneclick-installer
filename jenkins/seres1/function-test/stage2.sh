2b4de1088263b42f755ca8a547f48c44cc0002be
Arthur Zhang <zhangwfkl@gmail.com>
2025-06-12 09:51:02 +0800

    Init: Jenkins

--- jenkins/seres1/function-test/stage2.sh
+++ jenkins/seres1/function-test/stage2.sh
@@ -6,17 +6,16 @@ def stage1() {
 def stage2() {
 	withCredentials([usernamePassword(credentialsId: 'OrcaMesGit', passwordVariable: 'PAS', usernameVariable: 'USER')]) {
 	echo 'Execute remotely'
-        // sh '${MAVEN_HOME}/bin/mvn -s ./quality-alarm-engine/orca-settings.xml -pl "!bff,!data-acquisition,!manual-entry-system,!miot-interface,!multisystem-data-integration,!platform-data-monitor,!quality-alarm,!operation-monitor,!quality-archives,!quality-base-setting,!quality-monitor-board,!quality-statistics-analyze,!quality-statistics-analyze"  -Dnexus.pass=public -Dnexus.user=public clean deploy '
+        sh '${MAVEN_HOME}/bin/mvn -s ./quality-alarm-engine/orca-settings.xml -pl "!bff,!data-acquisition,!manual-entry-system,!miot-interface,!multisystem-data-integration,!platform-data-monitor,!quality-alarm,!operation-monitor,!quality-archives,!quality-base-setting,!quality-monitor-board,!quality-statistics-analyze,!quality-statistics-analyze"  -Dnexus.pass=public -Dnexus.user=public clean deploy '
 	sh 'cd quality-alarm-engine && pwd'
-	sh 'pwd'
-	echo 'stage2'
+	echo 'Stage2 end.'
         }
 }
 
 def stage3() {
 	def aliyun = 'registry.cn-hangzhou.aliyuncs.com/orca_zhang'
 	def docker = 'docker.io'
-	sh 'echo ${buildImage}'
+	sh 'echo ${buildImage}. Execute remotely'
                 if (buildImage) {
                    if (registry == 'aliyun') {
                        withCredentials([usernamePassword(credentialsId: 'OrcaAli', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
