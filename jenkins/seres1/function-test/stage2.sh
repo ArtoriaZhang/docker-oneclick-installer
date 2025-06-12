a5d48def257720139bc174a4de1f1bfaeaf93271
Arthur Zhang <zhangwfkl@gmail.com>
2025-06-12 09:26:21 +0800

    Init: Jenkins

--- jenkins/seres1/function-test/stage2.sh
+++ jenkins/seres1/function-test/stage2.sh
@@ -15,4 +15,33 @@ def stage2() {
 	echo 'stage2'
 }
 
+def stage3() {
+	def aliyun = 'registry.cn-hangzhou.aliyuncs.com/orca_zhang'
+	def docker = 'docker.io'
+	sh 'echo ${buildImage}'
+                if (buildImage) {
+                   if (registry == 'aliyun') {
+                       withCredentials([usernamePassword(credentialsId: 'OrcaAli', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
+                            // sh "cd quality-service/quality-server && docker build --load -t ${aliyun}/hollysys-seres-quality-server:${imageV} ./"
+                            sh 'docker login -u ${USERNAME} -p ${PASSWORD} registry.cn-hangzhou.aliyuncs.com'
+                            // sh "docker push ${aliyun}/hollysys-seres-quality-server:${imageV} "
+                            
+                            sh "cd quality-alarm-engine/engine-data-process && docker build --load -t ${aliyun}/hollysys-seres1-quality-engine-data:${engineV} ./"
+                            sh "docker push ${aliyun}/hollysys-seres1-quality-engine-data:${engineV} "
+                            
+                            sh "cd quality-alarm-engine/engine-passrate-alarm && docker build --load -t ${aliyun}/hollysys-seres1-quality-engine-passrate:${engineV} ./"
+                            sh "docker push ${aliyun}/hollysys-seres1-quality-engine-passrate:${engineV} "
+                            
+                            sh "cd quality-data-process && docker build --load -t ${aliyun}/hollysys-seres1-quality-data-process:${engineV} ./"
+                            sh "docker push ${aliyun}/hollysys-seres1-quality-data-process:${engineV} "
+                        }
+                   } else {
+                       withCredentials([usernamePassword(credentialsId: 'OrcaDocker', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
+                            sh "build image false"
+                            sh "docker build --load -t $USERNAME/hollysys-jiahua-mes-server-dev:${b} ./"
+                            sh "docker push $USERNAME/hollysys-jiahua-mes-server-dev:${b}" 
+                        }
+                   }
+   }
+
 return this
