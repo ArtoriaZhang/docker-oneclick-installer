365ec4a90f222adcf93069c8458ecf40a1d18601
Arthur Zhang <zhangwfkl@gmail.com>
2025-06-12 09:15:46 +0800

    Init: Jenkins

--- jenkins/seres1/function-test/stage2.sh
+++ jenkins/seres1/function-test/stage2.sh
@@ -5,6 +5,12 @@ def stage1() {
 
 def stage2() {
 
+	withCredentials([usernamePassword(credentialsId: 'OrcaMesGit', passwordVariable: 'PAS', usernameVariable: 'USER')]) {
+            sh '${MAVEN_HOME}/bin/mvn -s ./quality-alarm-engine/orca-settings.xml -pl "!bff,!data-acquisition,!manual-entry-system,!miot-interface,!multisystem-data-integration,!platform-data-monitor,!quality-alarm,!operation-monitor,!quality-archives,!quality-base-setting,!quality-monitor-board,!quality-statistics-analyze,!quality-statistics-analyze"  -Dnexus.pass=public -Dnexus.user=public clean deploy '
+            sh 'cd quality-alarm-engine && pwd'
+            sh 'pwd'
+            
+        }
 	echo 'stage2'
 }
 
