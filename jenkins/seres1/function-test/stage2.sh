734ed663d464666125aa21d6dc3d35446a591c3c
Arthur Zhang <zhangwfkl@gmail.com>
2025-07-15 15:58:09 +0800

    Shell: Jenkins: Seres1: Add seres-function-test-ui

--- jenkins/seres1/function-test/stage2.sh
+++ jenkins/seres1/function-test/stage2.sh
@@ -21,10 +21,10 @@ def stage1() {
 def stage2() {
 	withCredentials([usernamePassword(credentialsId: 'OrcaMesGit', passwordVariable: 'PAS', usernameVariable: 'USER')]) {
 	echo 'Execute remotely'
+	sh 'cd seres-function-test-ui && npm install && npm run build'
         sh '${MAVEN_HOME}/bin/mvn -s ./quality-alarm-engine/orca-settings.xml -pl "!bff,!data-acquisition,!manual-entry-system,!miot-interface,!multisystem-data-integration,!platform-data-monitor,!quality-alarm,!operation-monitor,!quality-archives,!quality-base-setting,!quality-monitor-board,!quality-statistics-analyze,!quality-statistics-analyze"  -Dnexus.pass=public -Dnexus.user=public clean deploy '
         sh 'cd seres-function-test && ${MAVEN_HOME}/bin/mvn  -Dnexus.pass=public -Dnexus.user=public clean package '
 	sh 'cd quality-alarm-engine && pwd'
-	sh 'cd seres-function-test-ui && npm run install && npm run build'
 	echo 'Stage2 end.'
         }
 }
